e55bf32444f0a5dedce2ae3704bdc9a220794618
Тамирлан <biktimirova.lera2@gmail.com>
2023-11-05 21:48:00 +0400

    Создание переменных

---
+++ src/DataTypes/Practice2.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        double b = 99.985;
+        System.out.println(b);
+
+        double x = 10;
+        System.out.println(x/3);
+
+        byte a = 127;
+        System.out.println(a);
+
+    }
+}
